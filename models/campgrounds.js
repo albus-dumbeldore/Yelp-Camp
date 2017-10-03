3ca4fcf31293803bdd0593f45ca8c6076cb9fd87
pkr31 <prafulkumar987@gmail.com>
2017-10-03 00:26:22 +0000

    update campground and comment schema

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -2,6 +2,8 @@ var mongoose=require("mongoose");
 var campgroundSchema=new mongoose.Schema({
     name       :String,
     price      :String,
+    createdAt: { type: Date, default: Date.now },
+  
     author     :{
                  id:{
                        type:mongoose.Schema.Types.ObjectId,
